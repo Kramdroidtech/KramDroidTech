b34f9cd40571f59328e6a2acff1026414e5bec81
Mark <markmarkifffff@gmail.com>
2020-08-10 22:08:44 -1200

    Code Check And Sass

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,37 +1,41 @@
-//Variables
-const header = document.querySelector('.header');
-const aboutImage = document.querySelector('.about-image');
-const aboutContent = document.querySelector('.about-content')
 
 //Fucntions
 window.addEventListener('DOMContentLoaded',()=>{
+    window.addEventListener('scroll',()=>{
 
+        const userSight = window.innerHeight;
+
+        //Animations
+        const animateHeader = () => {
+            const header = document.querySelector('.header');
+
+            if(scrollY > 20){
+                header.classList.add('startScroll');
+            }else{
+                header.classList.remove('startScroll');
+            }
+        }
+
+        const animateAbout = () => {
+            const aboutImage = document.querySelector('.about-image');
+            const aboutContent = document.querySelector('.about-content');
+            const aboutImagePost = aboutImage.getBoundingClientRect().top;
+        
+            if(aboutImagePost < userSight){
+                aboutImage.classList.add('showDownUp');
+                aboutContent.classList.add('showUpDown');
+            }
+            else{
+                aboutImage.classList.remove('showDownUp');
+                aboutContent.classList.remove('showUpDown');
+            }
+        
+        }
+        //Run Scroll Function
+        animateHeader();
+        animateAbout();
+  });
 });
 
-window.addEventListener('scroll',()=>{
-      animateHeader();
-      animate();
-});
 
-//Animations
-const animateHeader =()=>{
-    if(scrollY > 20){
-        header.classList.add('startScroll');
-    }else{
-        header.classList.remove('startScroll');
-    }
-}
-const animate = ()=>{
-    const aboutImagePost = aboutImage.getBoundingClientRect().top;
-    const userSight = window.innerHeight;
 
-    if(aboutImagePost < userSight){
-        aboutImage.classList.add('showDownUp');
-        aboutContent.classList.add('showUpDown');
-    }
-    else{
-        aboutImage.classList.remove('showDownUp');
-        aboutContent.classList.remove('showUpDown');
-    }
-
-}
