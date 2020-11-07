6096c9085df23b6d75fbc8cdb39fe6447098a57e
Mark Dacurawat <markmarkifffff@gmail.com>
2020-11-06 20:37:55 -1200

    Try TO Fix Bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,42 @@
-
 //Fucntions
-window.addEventListener('DOMContentLoaded',()=>{
-    window.addEventListener('scroll',()=>{
+window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", () => {
+    const userSight = window.innerHeight;
 
-        const userSight = window.innerHeight;
+    //Animations
+    const animateHeader = () => {
+      const header = document.querySelector(".header");
+      scrollY > 20
+        ? header.classList.add("startScroll")
+        : header.classList.remove("startScroll");
+    };
 
-        //Animations
-        const animateHeader = () => {
-            const header = document.querySelector('.header');
+    const animateAbout = () => {
+      const aboutImage = document.querySelector(".about-image");
+      const aboutContent = document.querySelector(".about-content");
+      const aboutImagePost = aboutImage.getBoundingClientRect().top;
 
-            if(scrollY > 20){
-                header.classList.add('startScroll');
-            }else{
-                header.classList.remove('startScroll');
-            }
-        }
-
-        const animateAbout = () => {
-            const aboutImage = document.querySelector('.about-image');
-            const aboutContent = document.querySelector('.about-content');
-            const aboutImagePost = aboutImage.getBoundingClientRect().top;
-        
-            if(aboutImagePost < userSight){
-                aboutImage.classList.add('showDownUp');
-                aboutContent.classList.add('showUpDown');
-            }
-            else{
-                aboutImage.classList.remove('showDownUp');
-                aboutContent.classList.remove('showUpDown');
-            }
-        
-        }
-        //Run Scroll Function
-        animateHeader();
-        animateAbout();
+      if (aboutImagePost < userSight) {
+        aboutImage.classList.add("showDownUp");
+        aboutContent.classList.add("showUpDown");
+      } else {
+        aboutImage.classList.remove("showDownUp");
+        aboutContent.classList.remove("showUpDown");
+      }
+    };
+    //Run Scroll Function
+    animateHeader();
+    animateAbout();
   });
 
-    
-    const navigationBar = () => {
-        const toggleBtn = document.querySelector('.toggle-btn');
-        const navigation = document.querySelector('.menu');
+  const navigationBar = () => {
+    const toggleBtn = document.querySelector(".toggle-btn");
+    const navigation = document.querySelector(".menu");
 
-        toggleBtn.addEventListener('click',() => {
-            navigation.classList.toggle('showMenu');
-        });
-    }
+    toggleBtn.addEventListener("click", () => {
+      navigation.classList.toggle("showMenu");
+    });
+  };
 
-    navigationBar();
+  navigationBar();
 });
-
-
-
